2e1943fac635adf8a65712208e85dccff54563e5
Babin6139 <khatribabin39@gmail.com>
2022-02-12 22:47:02 +0545

    some design changes

--- client/customer/lib/pages/login_page.dart
+++ client/customer/lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:customer/utils/mycolor.dart';
 import 'package:customer/utils/routes.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -9,95 +10,128 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Material(
-      child: Stack(clipBehavior: Clip.none, children: [
-        Center(
-          child: Container(
-            height: 300.0,
-            width: 200.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black45,
-                  offset: Offset(5.0, 5.0),
-                  blurRadius: 10.0,
+      child: Container(
+        decoration: BoxDecoration(
+            //     gradient: LinearGradient(
+            //   begin: Alignment.topLeft,
+            //   end: Alignment.bottomRight,
+            //   colors: [Colors.blueAccent, Colors.green, Colors.black38],
+            // )
+            ),
+        child: SafeArea(
+          child: Stack(clipBehavior: Clip.none, children: [
+            Positioned(
+              width: size.width,
+              top: 10.0,
+              child: Text(
+                "Saman Pathao",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: MyColor.color1),
+              ),
+            ),
+            Center(
+              child: Container(
+                height: 300.0,
+                width: 200.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: MyColor.color1,
+                      offset: Offset(5.0, 5.0),
+                      blurRadius: 10.0,
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    "Log In",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        label: Text("Email"), hintText: "Enter your email"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        label: Text("Password"),
-                        hintText: "Enter your password"),
-                  ),
-                  SizedBox(
-                    height: 6.0,
-                  ),
-                  Material(
-                    color: Colors.purpleAccent,
-                    child: InkWell(
-                      onTap: () {},
-                      splashColor: Colors.purple,
-                      child: Container(
-                        width: 50,
-                        height: 40,
-                        alignment: Alignment.center,
-                        child: Text("Log In"),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Log In",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            label: Text("Email / Username",
+                                style: TextStyle(color: MyColor.color1)),
+                            hintText: "Enter your email"),
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            label: Text("Password",
+                                style: TextStyle(color: MyColor.color1)),
+                            hintText: "Enter your password"),
+                      ),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Material(
+                        borderRadius: BorderRadius.circular(100),
+                        color: MyColor.color1,
+                        child: InkWell(
+                          onTap: () {},
+                          splashColor: Colors.purple,
+                          child: Container(
+                            width: 100,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: Text("Log In"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      RichText(
+                          text: TextSpan(
+                              style: TextStyle(color: Colors.black),
+                              children: [
+                            TextSpan(text: "Not registered yet? "),
+                            TextSpan(
+                                text: "Sign Up",
+                                style: TextStyle(color: Colors.blue),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.pushNamed(
+                                        context, MyRoutes.signup);
+                                  })
+                          ]))
+                    ],
                   ),
-                  SizedBox(
-                    height: 6.0,
-                  ),
-                  RichText(
-                      text: TextSpan(
-                          style: TextStyle(color: Colors.black),
-                          children: [
-                        TextSpan(text: "Not registered yet? "),
-                        TextSpan(
-                            text: "Sign Up",
-                            style: TextStyle(color: Colors.blue),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.pushNamed(context, MyRoutes.signup);
-                              })
-                      ]))
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-        Positioned(
-            top: size.height / 2 - 210,
-            left: size.width / 2 - 40.0,
-            child: Container(
-              clipBehavior: Clip.antiAlias,
-              width: 80.0,
-              height: 80.0,
-              child: Image.asset(
-                "assets/images/login.png",
-                fit: BoxFit.cover,
+            Positioned(
+              top: size.height / 2 - 220,
+              left: size.width / 2 - 40.0,
+              child: CircleAvatar(
+                maxRadius: 40,
+                backgroundImage: AssetImage("assets/images/login.png"),
               ),
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(40.0)),
-            )),
-      ]),
+            )
+            // child: Container(
+            //   clipBehavior: Clip.antiAlias,
+            //   width: 80.0,
+            //   height: 80.0,
+            //   child: Image.asset(
+            //     "assets/images/login.png",
+            //     fit: BoxFit.cover,
+            //   ),
+            //   decoration:
+            //       BoxDecoration(borderRadius: BorderRadius.circular(40.0)),
+            // )),
+          ]),
+        ),
+      ),
     );
   }
 }
--- client/customer/lib/pages/signup_page.dart
+++ client/customer/lib/pages/signup_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:customer/models/users.dart';
+import 'package:customer/utils/mycolor.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -38,182 +39,214 @@ class _SignUpState extends State<SignUp> {
     Size size = MediaQuery.of(context).size;
     return Material(
       child: SingleChildScrollView(
-        child: Stack(clipBehavior: Clip.none, children: [
-          Center(
-            child: Container(
-              margin: EdgeInsets.only(top: 140, bottom: 40.0),
-              width: 200.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black45,
-                    offset: Offset(5.0, 5.0),
-                    blurRadius: 10.0,
+        child: Container(
+          decoration: BoxDecoration(
+              //     gradient: LinearGradient(
+              //   begin: Alignment.topLeft,
+              //   end: Alignment.bottomRight,
+              //   colors: [Colors.blueAccent, Colors.green, Colors.black38],
+              // )),
+              ),
+          child: SafeArea(
+            child: Stack(clipBehavior: Clip.none, children: [
+              Positioned(
+                width: size.width,
+                top: 10.0,
+                child: Text(
+                  "Saman Pathao",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: MyColor.color1),
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 140, bottom: 40.0),
+                  width: 200.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: MyColor.color1,
+                        offset: Offset(5.0, 5.0),
+                        blurRadius: 10.0,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.firstName = value;
-                        });
-                      },
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                          label: Text("First name"),
-                          hintText: "Enter your first name"),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.middleName = value;
-                        });
-                      },
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                          label: Text("Middle name"),
-                          hintText: "Enter you middle name"),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.lastName = value;
-                        });
-                      },
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                          label: Text("Last name"),
-                          hintText: "Enter your last name"),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.email = value;
-                        });
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          label: Text("Email"), hintText: "Enter your email"),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.contactNo = value;
-                        });
-                      },
-                      keyboardType: TextInputType.phone,
-                      maxLength: 10,
-                      decoration: InputDecoration(
-                          label: Text("Contact no"),
-                          hintText: "Enter your contact no"),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.address = value;
-                        });
-                      },
-                      decoration: InputDecoration(
-                          label: Text("Address"),
-                          hintText: "Enter your address"),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    GestureDetector(
-                      onTap: pickImage,
-                      child: AnimatedContainer(
-                        clipBehavior: Clip.antiAlias,
-                        duration: Duration(seconds: 1),
-                        decoration: BoxDecoration(
-                            color: Colors.amber,
-                            borderRadius: checkPhoto
-                                ? BorderRadius.circular(100)
-                                : BorderRadius.circular(0)),
-                        width: checkPhoto ? 60 : 200.0,
-                        height: checkPhoto ? 60 : 30.0,
-                        alignment: Alignment.center,
-                        child: user.photo != null
-                            ? Image.file(File(tempImage))
-                            : Text("Pick Image"),
-                      ),
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          user.password = value;
-                        });
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          label: Text("Password"),
-                          hintText: "Enter your password"),
-                    ),
-                    SizedBox(
-                      height: 6.0,
-                    ),
-                    Material(
-                      color: Colors.purpleAccent,
-                      child: InkWell(
-                        onTap: signUp,
-                        splashColor: Colors.purple,
-                        child: Container(
-                          width: 50,
-                          height: 40,
-                          alignment: Alignment.center,
-                          child: Text("Sign Up"),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.firstName = value;
+                            });
+                          },
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                              label: Text("First name",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter your first name"),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.middleName = value;
+                            });
+                          },
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                              label: Text("Middle name",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter you middle name"),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.lastName = value;
+                            });
+                          },
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                              label: Text("Last name",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter your last name"),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.email = value;
+                            });
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              label: Text("Email",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter your email"),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.contactNo = value;
+                            });
+                          },
+                          keyboardType: TextInputType.phone,
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                              label: Text("Contact no",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter your contact no"),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.address = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                              label: Text("Address",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter your address"),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        GestureDetector(
+                          onTap: pickImage,
+                          child: AnimatedContainer(
+                            clipBehavior: Clip.antiAlias,
+                            duration: Duration(seconds: 1),
+                            decoration: BoxDecoration(
+                                color: MyColor.color1,
+                                borderRadius: checkPhoto
+                                    ? BorderRadius.circular(100)
+                                    : BorderRadius.circular(0)),
+                            width: checkPhoto ? 60 : 200.0,
+                            height: checkPhoto ? 60 : 30.0,
+                            alignment: Alignment.center,
+                            child: user.photo != null
+                                ? Image.file(File(tempImage))
+                                : Text("Pick Image"),
+                          ),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              user.password = value;
+                            });
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              label: Text("Password",
+                                  style: TextStyle(color: MyColor.color1)),
+                              hintText: "Enter your password"),
+                        ),
+                        SizedBox(
+                          height: 6.0,
+                        ),
+                        Material(
+                          color: MyColor.color1,
+                          borderRadius: BorderRadius.circular(100),
+                          child: InkWell(
+                            onTap: signUp,
+                            splashColor: Colors.purple,
+                            child: Container(
+                              width: 100,
+                              height: 40,
+                              alignment: Alignment.center,
+                              child: Text("Sign Up"),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 6.0,
+                        ),
+                        RichText(
+                            text: TextSpan(
+                                style: TextStyle(color: Colors.black),
+                                children: [
+                              TextSpan(text: "Not registered yet? "),
+                              TextSpan(
+                                  text: "Log In",
+                                  style: TextStyle(color: Colors.blue),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      print("Hello");
+                                    })
+                            ]))
+                      ],
                     ),
-                    SizedBox(
-                      height: 6.0,
-                    ),
-                    RichText(
-                        text: TextSpan(
-                            style: TextStyle(color: Colors.black),
-                            children: [
-                          TextSpan(text: "Not registered yet? "),
-                          TextSpan(
-                              text: "Log In",
-                              style: TextStyle(color: Colors.blue),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  print("Hello");
-                                })
-                        ]))
-                  ],
+                  ),
                 ),
               ),
-            ),
+              Positioned(
+                  top: size.height / 2 - 220,
+                  left: size.width / 2 - 40.0,
+                  child: Container(
+                    clipBehavior: Clip.antiAlias,
+                    width: 80.0,
+                    height: 80.0,
+                    child: Image.asset(
+                      "assets/images/login.png",
+                      fit: BoxFit.cover,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40.0)),
+                  )),
+            ]),
           ),
-          Positioned(
-              top: size.height / 2 - 210,
-              left: size.width / 2 - 40.0,
-              child: Container(
-                clipBehavior: Clip.antiAlias,
-                width: 80.0,
-                height: 80.0,
-                child: Image.asset(
-                  "assets/images/login.png",
-                  fit: BoxFit.cover,
-                ),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(40.0)),
-              )),
-        ]),
+        ),
       ),
     );
   }
